dc472c9f29c0a8cab2e94c9b8b20d42b2c48bd9d
ShyamB97 <bhuller.shyam@gmail.com>
2025-05-30 15:57:08 +0100

    add git ignore

--- tools/create_analysis_area.sh
+++ tools/create_analysis_area.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
+cd $SCRIPT_DIR/..
+
 git clone https://github.com/bristol-dune/pi0-analysis.git
 cd pi0-analysis
 git checkout sbhuller/python-code # The current branch that is most up-to-date
--- tools/create_dunesw.sh
+++ tools/create_dunesw.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
-cd .. # create in main area
+cd $SCRIPT_DIR/.. # create in main area
 
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 mkdir dune
