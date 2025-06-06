277f7403ad9f55166f7bcb97e79bdf3c639aef26
Wanyun Su <suw@pplxint12.physics.ox.ac.uk>
2025-06-06 15:13:56 +0100

    use ssh clone

--- tools/create_analysis_area.sh
+++ tools/create_analysis_area.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
 cd $SCRIPT_DIR/..
 
-git clone https://github.com/bristol-dune/pi0-analysis.git
+git clone git@github.com:bristol-dune/pi0-analysis.git 
 cd pi0-analysis
 git checkout sbhuller/python-code # The current branch that is most up-to-date
